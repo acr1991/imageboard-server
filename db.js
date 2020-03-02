9fa88071f1379b8d1d75707f02d0ba8474de928f
acr1991 <acr1991@live.nl>
2020-03-02 14:09:02 +0100

    Router initated

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const databaseUrl =
   process.env.DATABASE_URL ||
-  "postgres://postgres:secret@localhost:5432/postgres";
+  "postgres://postgres:postgres@localhost:5432/postgres";
 const db = new Sequelize(databaseUrl);
 db.sync()
   .then(() => console.log("Database Updated"))
--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const express = require("express");
 const port = process.env.PORT || 4000;
 const app = express();
 const db = require("./db");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+
+const imageRouter = require("../image/router");
+app.use(jsonParser);
+app.use(imageRouter);
 
 app.listen(port, () => console.log(`Listening on :${port}`));
