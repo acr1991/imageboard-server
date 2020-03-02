6b2379205b981eb0bf7bf90e8c1a10c491dd3e63
acr1991 <acr1991@live.nl>
2020-03-02 11:52:07 +0100

    db file added to project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const port = process.env.PORT || 4000;
 const app = express();
+const db = require("./db");
 
 app.listen(port, () => console.log(`Listening on :${port}`));
