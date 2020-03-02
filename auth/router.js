3002cb418e668576ac3d6d7c89ddb4be94dfb02e
acr1991 <acr1991@live.nl>
2020-03-02 16:32:51 +0100

    Assignment finalized

--- auth/router.js
+++ auth/router.js
@@ -3,6 +3,7 @@ const { toJWT, toData } = require("./jwt");
 const router = new Router();
 
 // define endpoints here
+
 // Login endpoint
 router.post("/login", (req, res, next) => {
   const email = req.body.email;
@@ -14,39 +15,55 @@ router.post("/login", (req, res, next) => {
   } else {
     User.findOne({
       where: { email: req.body.email }
-    }).then(entity => {
-      if (!entity) {
-        res.status(400).send({
-          message: "Username or Email does not exist."
+    })
+      .then(entity => {
+        if (!entity) {
+          res.status(400).send({
+            message: "Username or Email does not exist."
+          });
+        } else if (bcrypt.compareSync(req.body.password, entity.password)) {
+          //if the password is true, return JWT with the userId of the user (user.id)
+          res.send({
+            jwt: toJWT({ userId: entity.id })
+          });
+        } else {
+          res.status(400).send({
+            message: "Password was incorrect"
+          });
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send({
+          message: "Something went wrong"
+        });
+      });
+    // Secret endpoint
+    router.get("/secret-endpoint", (req, res) => {
+      const auth =
+        req.headers.authorization && req.headers.authorization.split(" ");
+      if (auth && auth[0] === "Bearer" && auth[1]) {
+        try {
+          const data = toData(auth[1]);
+          res.send({
+            message: "Thanks for visiting the secret endpoint.",
+            data
+          });
+        } catch (error) {
+          res.status(400).send({
+            message: `Error ${error.name}: ${error.message}`
+          });
+        }
+      } else {
+        res.status(401).send({
+          message: "Please supply some valid credentials"
         });
       }
     });
+    // res.send({
+    //   jwt: toJWT({ userId: 1 })
+    // });
   }
 });
-// Secret endpoint
-router.get("/secret-endpoint", (req, res) => {
-  const auth =
-    req.headers.authorization && req.headers.authorization.split(" ");
-  if (auth && auth[0] === "Bearer" && auth[1]) {
-    try {
-      const data = toData(auth[1]);
-      res.send({
-        message: "Thanks for visiting the secret endpoint.",
-        data
-      });
-    } catch (error) {
-      res.status(400).send({
-        message: `Error ${error.name}: ${error.message}`
-      });
-    }
-  } else {
-    res.status(401).send({
-      message: "Please supply some valid credentials"
-    });
-  }
-});
-// res.send({
-//   jwt: toJWT({ userId: 1 })
-// });
 
 module.exports = router;
